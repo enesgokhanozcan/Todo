576d7291992f00341dadb0d4800ccb20557e9306
enesgokhanozcan <enes.gokhan.ozcan@gmail.com>
2022-03-27 11:19:13 +0300

    tasks list and css bugs fix.

--- fe/src/components/TodoItem.tsx
+++ fe/src/components/TodoItem.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { TodoSchema } from "../model";
+import "./TodoItem.css";
+import { MdDone } from "react-icons/md";
 
 interface Props {
-    todo: TodoSchema;
-    todos: TodoSchema[];
-    setTodos: React.Dispatch<React.SetStateAction<TodoSchema[]>>;
-  }
+  todo: TodoSchema;
+  todos: TodoSchema[];
+  setTodos: React.Dispatch<React.SetStateAction<TodoSchema[]>>;
+}
 
 const TodoItem: React.FC<Props> = ({ todo, todos, setTodos }) => {
-  return(
-    <p>Hello</p>
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
+  };
+  return (
+    <form className="todoItem">
+      <div>
+        <span className="icon_done" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
+      {todo.isCompleted ? <s>{todo.todo}</s> : <span>{todo.todo}</span>}
+    </form>
   );
 };
 
